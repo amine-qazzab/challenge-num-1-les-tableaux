0581e9f017bc17daf8a536131d6b67a487d7777b
amine-qazzab <amineqazzab646@gmail.com>
2024-09-26 15:29:22 +0100

    challenge num 1 les tableaux

---
+++ challenge1.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    float T [5];
+    int i;
+    for(i=0 ; i<5 ; i++) {
+    printf("entrez le nombre %d : ",i+1);
+    scanf("%f" , &T[i]);
+}
+    return 0;
+}
